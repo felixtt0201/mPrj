8f0e23bdffc5b768156ef179dc15511504faf0af
Bentt0201 <bem0507@hotmail.com>
2020-10-22 14:37:59 +0800

    修正註冊功能

--- js/register.js
+++ js/register.js
@@ -9,28 +9,41 @@ let accountData = [];
 function register(e) {
   e.preventDefault();
   let acEmail = "";
-  let acAccount = ""
+  let acAccount = "";
   axios.get(accountURL).then(res => {
     accountData = res.data;
     accountData.forEach(i => {
       acEmail = i.email;
       acAccount = i.account;
-      if (inputEmail.value === acEmail) {
-        alert('重複信箱')
-        return
-      }
-      else if (inputAccount.value === acAccount) {
-        alert('重複帳號')
-        return
-      }
-    });
-    axios.post(accountURL, {
-      name: inputName.value,
-      email: inputEmail.value,
-      account: inputAccount.value,
-      password: inputPassword.value
     })
-    console.log('成功註冊');
+    if (!inputName.value) {
+      alert('請填寫姓名')
+    }
+    else if (!inputEmail.value) {
+      alert('請填寫信箱')
+    }
+    else if (!inputAccount.value) {
+      alert('不能為空')
+    }
+    else if (!inputPassword.value) {
+      alert('不能為空')
+    }
+    else if (inputEmail.value === acEmail) {
+      alert('重複信箱')
+    }
+    else if (inputAccount.value === acAccount) {
+      alert('重複帳號')
+
+    }
+    else {
+      axios.post(accountURL, {
+        name: inputName.value,
+        email: inputEmail.value,
+        account: inputAccount.value,
+        password: inputPassword.value
+      })
+      console.log('成功註冊');
+    }
   })
 };
 submit.addEventListener('click', register)
