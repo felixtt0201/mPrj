5bdf6bcd6edba129108b229fe1115e491a059aed
KentShen <observer1220@gmail.com>
2020-10-26 17:25:18 +0800

    更新reset內容

--- js/content.js
+++ js/content.js
@@ -11,9 +11,9 @@ var simplemde = new SimpleMDE({
     },
     element: document.getElementById("MyID"),
     forceSync: true,
-    hideIcons: ["guide", "heading", "side-by-side", "fullscreen"],
+    hideIcons: ["guide", "heading"],
     indentWithTabs: false,
-    initialValue: " ",
+    initialValue: "",
     insertTexts: {
         horizontalRule: ["", "\n\n-----\n\n"],
         image: ["![](http://", ")"],
@@ -26,7 +26,7 @@ var simplemde = new SimpleMDE({
         strikethrough: false,
         underscoresBreakWords: true,
     },
-    placeholder: "Type here...",
+    placeholder: "快來寫下你的回答...",
     previewRender: function (plainText) {
         return customMarkdownParser(plainText); // Returns HTML from a custom parser
     },
@@ -45,7 +45,43 @@ var simplemde = new SimpleMDE({
     shortcuts: {
         drawTable: "Cmd-Alt-T"
     },
-    showIcons: ["bold", "italic", "link", "quote", "code", "image", "|", "ordered-list", "unordered-list", "horizontal-rule", "|", "preview"],
+    showIcons: ["code", "table"],
     spellChecker: false,
     status: false,
+    status: ["autosave", "lines", "words", "cursor"], // Optional usage
+    status: ["autosave", "lines", "words", "cursor", {
+        className: "keystrokes",
+        defaultValue: function (el) {
+            this.keystrokes = 0;
+            el.innerHTML = "0 Keystrokes";
+        },
+        onUpdate: function (el) {
+            el.innerHTML = ++this.keystrokes + " Keystrokes";
+        }
+    }], // Another optional usage, with a custom status bar item that counts keystrokes
+    // styleSelectedText: false,
+    // tabSize: 4,
+    // toolbar: false,
+    // toolbarTips: false,
 });
+
+
+// 文章頁面的留言功能
+let leaveMessage = document.getElementById('leaveMessage');
+leaveMessage.addEventListener('click', function () {
+    // axios.get(`https://fierce-forest-92782.herokuapp.com/account/${}`)
+
+    let _date = new Date();
+    let date = _date.getFullYear() + '/' + (_date.getMonth() + 1) + '/' + _date.getDate();
+    let time = _date.getHours() + ':' + _date.getMinutes() + ':' + _date.getSeconds();
+    let getContent = document.getElementById('MyID').value;
+
+    axios.post('https://fierce-forest-92782.herokuapp.com/message', {
+        id: '',
+        mName: '阿瀚',
+        date: `${date} ${time}`,
+        mcontent: `${getContent}`,
+        msgOwenerID: 'sfsfsfsf',
+    })
+    console.log('點到了')
+}, false)
