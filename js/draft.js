7708061a26ddd585bf2222a0a0ca4092e37b9a91
りんりん <chenchenssssss@gmail.com>
2020-10-27 14:53:56 +0800

    新增發文功能

--- js/draft.js
+++ js/draft.js
@@ -1,15 +1,17 @@
 const draft_title = document.getElementById('draft-header-title');
 const draft_btn = document.querySelector('.btn-draft');
+const draft_api ='https://fierce-forest-92782.herokuapp.com/articles';
+
 // console.log(draft_btn)
 // console.log(draft_title)
+
 let draft_obj ={
   title: '',
   author: '',
   data: '',
-  view: '',
   content: '',
-  like: '',
-  artOnwerID: ''
+  artOnwerID: '',
+  articleID:''
 }
 draft();
 
@@ -18,19 +20,36 @@ function draft(){
   /* 取得title */
   let title = draft_title.value;
   draft_obj.title = title ;
-  console.log(draft_obj.title)
+  // console.log(draft_obj.title)
 
   /* 取得時間 */
   const date = new Date();
   const nowDay = date.toLocaleString();
   draft_obj.data = nowDay;
 
+  /* 取得文章內容 */
   draft_obj.content = CKEDITOR.instances['editor-main'].getData();
-  console.log(draft_obj.content)
+  // console.log(draft_obj.content)
 
+  /* 個人ID & 作者名字 */
+  let getStatus = localStorage.getItem('loginStatus');
+  let parseStatus = JSON.parse(getStatus);
+  // console.log(parseStatus)
+  draft_obj.artOnwerID = parseStatus.loginID;
+  draft_obj.author = parseStatus.loginName ;
+
+  /* 文章ID */
+  let today = Date.parse(nowDay);
+  draft_obj.articleID = `${parseStatus.loginID}${today}`;
+  console.log(draft_obj.articleID);
 }
 
-console.log(draft_obj.data)
-console.log(draft_obj.title)
+function post_draft(){
+  draft();
+  axios.post(draft_api,draft_obj)
+  .then(res=>{
+    console.log (res);
+  })
+}
 
-draft_btn.addEventListener('click',draft)
+draft_btn.addEventListener('click',post_draft)
